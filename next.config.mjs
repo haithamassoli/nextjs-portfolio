3d5140508478ad6959d5ae9e4aa08eda855c75c4
haithamassoli <haitham.b.assoli@gmail.com>
2024-09-29 09:43:21 +0300

    configure TypeScript to ignore build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.(".svg")
+      rule.test?.test?.(".svg"),
     );
 
     config.module.rules.push(
@@ -35,7 +38,7 @@ const nextConfig = {
             },
           },
         },
-      }
+      },
     );
 
     // Modify the file loader rule to ignore *.svg, since we have it handled now.
