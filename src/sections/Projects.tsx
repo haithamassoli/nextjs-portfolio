1fd3f16f1c9ecd322ff1df2f154d98147da6e5fe
haithamassoli <haitham.b.assoli@gmail.com>
2024-09-29 09:31:55 +0300

    projects section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,134 +1,239 @@
-import promptShareImage from "@/assets/images/promptShareImage.png";
-import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
-import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
-import Image from "next/image";
-import CheckCircleIcon from "@/assets/icons/check-circle.svg";
-import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
-import grainImage from "@/assets/images/grain.jpg";
-import iphoneLandingImage from "@/assets/images/iphoneLandingImage.png";
+"use client";
 
-const portfolioProjects = [
+import SectionHeader from "@/components/SectionHeader";
+import { useState } from "react";
+import GitHubIcon from "@/assets/icons/github-outline";
+import FolderIcon from "@/assets/icons/folder";
+import ExternalIcon from "@/assets/icons/external";
+import PlayStoreIcon from "@/assets/icons/playStore";
+
+const projectsArr = [
   {
-    company: "Innovative Co",
-    year: "2021",
-    title: "Light Saas Landing Page",
-    results: [
-      { title: "Boosted sales by 20%" },
-      { title: "Expanded customer reach by 35%" },
-      { title: "Increased brand awareness by 15%" },
+    title: "Kheir",
+    techStack: [
+      "React Native",
+      "TypeScript",
+      "Zod",
+      "Firebase",
+      "Zustand",
+      "React Query",
+      "Reanimated",
+      "Restyle",
     ],
-    link: "https://saa-s-nextjs-project.vercel.app/",
-    image: lightSaasLandingPage,
+    description: "An application to donate to the needy and help them.",
+    playStore:
+      "https://play.google.com/store/apps/details?id=com.haithamassoli.kheir",
+    github: "https://github.com/haithamassoli/kheir",
   },
   {
-    company: "Quantum Dynamics",
-    year: "2023",
-    title: "AI Startup Landing Page",
-    results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
-    ],
-    link: "https://frontend-ai-startup-landing-page.vercel.app/",
-    image: aiStartupLandingPage,
+    title: "Azkari",
+    techStack: ["Javascript", "VSCode Extension"],
+    description:
+      "VSCode extension to remind you to remember Allah every now and then.",
+    external:
+      "https://marketplace.visualstudio.com/items?itemName=HaithamAssoli.azkari",
+    github: "https://github.com/haithamassoli/Azkari",
   },
   {
-    company: "PromptShare AI",
-    year: "2024",
-    title: "AI Prompt Sharing Platform",
-    results: [
-      { title: "Increased community engagement by 50%" },
-      { title: "Streamlined prompt-sharing process by 30%" },
-      { title: "Expanded user base by 40%" },
+    title: "Marafiq+",
+    techStack: [
+      "React Native",
+      "TypeScript",
+      "Google Maps",
+      "Zod",
+      "Firebase",
+      "Zustand",
+      "React Query",
+      "Reanimated",
+      "Restyle",
     ],
-    link: "https://project-next-14-ai-prompt-sharing-self.vercel.app/",
-    image: promptShareImage,
+    description:
+      "An application that helps you find parks and public facilities.",
+    playStore:
+      "https://play.google.com/store/apps/details?id=com.haithamassoli.hadiqa",
+    github: "https://github.com/haithamassoli/hadiqa",
   },
   {
-    company: "iPhone 15 Pro",
-    year: "2024",
-    title: "iPhone 15 Shopify Page",
-    results: [],
-    link: "https://iphone-blush-chi.vercel.app/",
-    image: iphoneLandingImage, // Replace with actual image when available
-    status: "In progress", // Optional status field
+    title: "Sawt",
+    techStack: [
+      "React Native",
+      "TypeScript",
+      "OTP",
+      "Zod",
+      "Firebase",
+      "Zustand",
+      "React Query",
+      "Reanimated",
+      "Restyle",
+    ],
+    description:
+      "Implementing an innovative technological solution that aims to automate and facilitate election processes.",
+    playStore:
+      "https://play.google.com/store/apps/details?id=com.haithamassoli.sawt",
+    github: "https://github.com/haithamassoli/Sawt",
+  },
+  {
+    title: "Boost Me",
+    techStack: [
+      "Javascript",
+      "React",
+      "Php",
+      "Laravel",
+      "MySQL",
+      "Tailwind CSS",
+    ],
+    description:
+      "Book a professional player to help you perform difficult tasks in games.",
+    github: "https://github.com/haithamassoli/Boost-me",
+  },
+  {
+    title: "Online Quizzes",
+    techStack: ["Javascript", "Php", "Laravel", "MySQL", "API", "Sass"],
+    description: "Website for managing and performing online exams.",
+    github: "https://github.com/haithamassoli/Online-Quiz-Laravel",
+  },
+  {
+    title: "Booking System",
+    techStack: [
+      "Javascript",
+      "React",
+      "Node.Js",
+      "Express",
+      "MongoDB",
+      "Tailwind CSS",
+    ],
+    description: "Restaurant table reservation system.",
+    github: "https://github.com/haithamassoli/Booking-System",
+  },
+  {
+    title: "Coza Store",
+    techStack: ["Javascript", "Php", "MySQL", "Sass"],
+    description: "Online store for the latest clothes and accessories.",
+    github: "https://github.com/haithamassoli/Coza-Store-PHP",
+  },
+  {
+    title: "Food Funday Restaurant",
+    techStack: ["Javascript", "React", "Tailwind CSS", "Sass"],
+    description:
+      "A system for reserving a restaurant table with food or taking the order upon arrival at the restaurant.",
+    github: "https://github.com/haithamassoli/Food-Funday-Restaurant",
+  },
+  {
+    title: "Amazon Clone",
+    techStack: ["Javascript", "React", "Firebase", "MUI"],
+    description:
+      "E-commerce replicates the familiar layout and design of Amazon, allowing users to browse and purchase products.",
+    github: "https://github.com/haithamassoli/Amazon-Clone",
   },
 ];
 
-export const ProjectsSection = () => {
-  return (
-    <section className="pb-16 lg:py-28">
-      <div className="container">
-        <h2 className="mt-6 text-center font-acorn text-3xl font-bold text-[#8fdcce] md:text-5xl">
-          Featured Projects
-        </h2>
-        <p className="mx-auto mt-4 max-w-md text-center text-white/80 md:text-lg lg:text-xl">
-          See how I transformed concepts into engaging digital experiences.
-        </p>
-        <div className="mt-10 flex flex-col gap-20 md:mt-20">
-          {portfolioProjects.map((project, projectIndex) => (
-            <div
-              key={project.title}
-              className="sticky z-0 overflow-hidden rounded-3xl bg-gray-800 px-8 pt-8 after:pointer-events-none after:absolute after:inset-0 after:z-10 after:rounded-3xl after:outline after:outline-2 after:-outline-offset-2 after:outline-white/20 after:content-[''] md:px-10 md:pt-12 lg:px-20 lg:pt-16"
-              style={{
-                top: `calc(64px + ${projectIndex * 40}px)`,
-              }}
-            >
-              <div
-                className="absolute inset-0 -z-10 opacity-5"
-                style={{
-                  backgroundImage: `url(${grainImage.src})`,
-                }}
-              ></div>
-              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
-                <div className="lg:pb-16">
-                  <div className="inline-flex gap-2 bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-sm font-bold uppercase tracking-widest text-transparent">
-                    <span>{project.company}</span>
-                    <span>&bull;</span>
-                    <span>{project.year}</span>
-                  </div>
-                  <h3 className="mt-2 font-acorn text-2xl md:mt-5 md:text-4xl">
-                    {project.title}
-                  </h3>
-                  <hr className="mt-4 border-t-2 border-white/5 md:mt-5" />
-                  <ul className="mt-4 flex flex-col gap-4 md:mt-5">
-                    {project.results.length > 0 ? (
-                      project.results.map((result) => (
-                        <li
-                          key={result.title}
-                          className="flex max-w-full items-center gap-2 overflow-hidden text-ellipsis whitespace-nowrap text-sm text-white/50 md:text-base"
-                        >
-                          <CheckCircleIcon className="h-5 w-5 md:h-6 md:w-6" />
-                          <span className="overflow-hidden text-ellipsis">
-                            {result.title}
-                          </span>
-                        </li>
-                      ))
-                    ) : (
-                      <li className="text-sm text-white/50 md:text-base">
-                        Project not yet completed
-                      </li>
-                    )}
-                  </ul>
-                  <a href={project.link} target="_blank">
-                    <button className="mt-8 inline-flex h-12 w-full items-center justify-center gap-2 rounded-xl bg-white px-6 font-semibold text-gray-950 md:w-auto">
-                      Visit Live Site
-                      <ArrowUpRightIcon className="h-4 w-4" />
-                    </button>
-                  </a>
-                </div>
-                <div className="relative">
-                  <Image
-                    src={project.image}
-                    alt={project.title}
-                    className="-mb-4 mt-8 md:-mb-0 lg:absolute lg:mt-0 lg:h-full lg:w-auto lg:max-w-none"
-                  />
-                </div>
-              </div>
+const GRID_LIMIT = 6;
+
+const Projects = () => {
+  const [showMore, setShowMore] = useState(false);
+
+  const firstSix = projectsArr.slice(0, GRID_LIMIT);
+  const projectsToShow = showMore ? projectsArr : firstSix;
+
+  const ProjectInner = ({ project }: any) => {
+    const { title, description, techStack, github, external, playStore } =
+      project;
+
+    return (
+      <div className="group flex h-full cursor-pointer flex-col justify-between rounded-lg bg-gray-800 p-7 shadow-lg transition-all duration-300 hover:translate-y-[-7px]">
+        <header>
+          <div className="mb-9 flex items-center justify-between">
+            <div className="h-10 w-10 text-secondary">
+              <FolderIcon />
             </div>
-          ))}
-        </div>
+            <div className="flex items-center space-x-2 text-gray-400">
+              {github && (
+                <a
+                  href={github}
+                  aria-label="GitHub Link"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="h-5 w-5 hover:text-secondary"
+                >
+                  <GitHubIcon />
+                </a>
+              )}
+              {playStore && (
+                <a
+                  href={playStore}
+                  aria-label="PlayStore Link"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="h-5 w-5 hover:text-secondary"
+                >
+                  <PlayStoreIcon />
+                </a>
+              )}
+              {external && (
+                <a
+                  href={external}
+                  aria-label="External Link"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="h-5 w-5 hover:text-secondary"
+                >
+                  <ExternalIcon />
+                </a>
+              )}
+            </div>
+          </div>
+
+          <h3 className="mb-2 text-2xl font-semibold text-gray-100">
+            <a
+              href={external}
+              target="_blank"
+              rel="noreferrer"
+              className="group-hover:text-secondary"
+            >
+              {title}
+            </a>
+          </h3>
+
+          <div
+            className="text-sm text-gray-300"
+            dangerouslySetInnerHTML={{ __html: description }}
+          />
+        </header>
+
+        <footer className="mt-5">
+          {techStack && (
+            <ul className="mt-5 flex flex-wrap gap-2 text-xs text-gray-400">
+              {techStack.map((item: string, i: number) => (
+                <li key={i}>{item}</li>
+              ))}
+            </ul>
+          )}
+        </footer>
+      </div>
+    );
+  };
+
+  return (
+    <section className="mx-8 py-16 md:container lg:py-24">
+      <SectionHeader
+        eyebrow="Other Noteworthy Projects"
+        description="Here are some of the projects I've worked on."
+      />
+      <div className="mt-10 grid w-full grid-cols-1 gap-4 md:mt-20 md:grid-cols-2 lg:grid-cols-3">
+        {projectsToShow.map((project, i) => (
+          <ProjectInner key={i} project={project} />
+        ))}
+      </div>
+      <div className="flex w-full justify-center">
+        <button
+          className="btn mx-auto mt-12 rounded border border-green-400 bg-transparent px-4 py-2 text-green-400 transition-all duration-300 hover:bg-green-400 hover:bg-opacity-10"
+          onClick={() => setShowMore(!showMore)}
+        >
+          Show {showMore ? "Less" : "More"}
+        </button>
       </div>
     </section>
   );
 };
+
+export default Projects;
