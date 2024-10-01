7244a28261020b46b69977ea4bf252845bce96b9
haithamassoli <haitham.b.assoli@gmail.com>
2024-10-01 11:17:48 +0300

    change framer animation to tailwind in navbar

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,63 +1,25 @@
-"use client";
-
-import { motion } from "framer-motion";
-
-const navVariants = {
-  hidden: { opacity: 0, y: -50 },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      duration: 0.6,
-      type: "spring",
-      stiffness: 120,
-    },
-  },
-};
-
 export const Header = () => {
-  const handleScrollToSection = (
-    sectionId: string,
-    event: React.MouseEvent<HTMLAnchorElement>,
-  ) => {
-    event.preventDefault();
-    const section = document.getElementById(sectionId);
-    if (section) {
-      const sectionTop = section.offsetTop;
-      const headerOffset = 80;
-
-      window.scrollTo({
-        top: sectionTop - headerOffset,
-        behavior: "smooth",
-      });
-    }
-  };
-
   return (
-    <div className="fixed top-8 z-50 flex w-full items-center justify-center">
-      <motion.nav
-        initial="hidden"
-        animate="visible"
-        variants={navVariants}
-        className="flex gap-1 rounded-full border border-white/15 bg-white/10 p-2 backdrop-blur"
-      >
+    <div className="fixed -top-16 z-50 flex w-full items-center justify-center">
+      <nav className="animate-fade-in flex gap-1 rounded-full border border-white/15 bg-white/10 p-2 backdrop-blur">
         {["Home", "Experience", "Projects", "About", "Contact"].map(
           (item, index) => (
             <a
               key={index}
-              href="#"
-              className={`nav-item ${item === "Experience" ? "hidden md:block" : ""} ${
+              href={`#${item.toLowerCase()}`}
+              className={`nav-item ${
+                item === "Experience" ? "hidden md:block" : ""
+              } ${
                 item === "Contact"
-                  ? "bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
-                  : ""
+                  ? "bg-white text-gray-900 hover:bg-white/70"
+                  : "hover:bg-white/10"
               }`}
-              onClick={(e) => handleScrollToSection(item.toLowerCase(), e)}
             >
               {item}
             </a>
           ),
         )}
-      </motion.nav>
+      </nav>
     </div>
   );
 };
