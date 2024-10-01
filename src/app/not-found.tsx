1fe1b63c38638518907f77024d967e7009f35881
haithamassoli <haitham.b.assoli@gmail.com>
2024-10-01 12:14:13 +0300

    feat: add custom 404 Not Found page with navigation to home

---
+++ src/app/not-found.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <title>Page Not Found</title>
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <h1 className="text-[clamp(100px,25vw,200px)] leading-none text-emerald-500">
+          404
+        </h1>
+        <h2 className="text-[clamp(30px,5vw,50px)] font-normal">
+          Page Not Found
+        </h2>
+        <Link href="/">
+          <div className="flex w-full justify-center">
+            <button className="btn mx-auto mt-12 rounded border border-green-400 bg-transparent px-4 py-2 text-green-400 transition-all duration-300 hover:bg-green-400 hover:bg-opacity-10">
+              Go Home
+            </button>
+          </div>
+        </Link>
+      </main>
+    </div>
+  );
+};
+
+export default NotFoundPage;
