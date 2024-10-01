8931f267087c947428a480fa500270c8a9004a75
haithamassoli <haitham.b.assoli@gmail.com>
2024-10-01 19:39:58 +0300

    animate card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,27 @@
 import { ComponentPropsWithoutRef } from "react";
-import grainImage from "@/assets/images/grain.jpg";
+import { motion } from "framer-motion";
 import { twMerge } from "tailwind-merge";
+import grainImage from "@/assets/images/grain.jpg";
 
 const Card = ({
   className,
   children,
+  delay,
   ...other
-}: ComponentPropsWithoutRef<"div">) => {
+}: ComponentPropsWithoutRef<any> & {
+  delay?: number;
+}) => {
   return (
-    <div
+    <motion.div
       className={twMerge(
         "relative z-0 overflow-hidden rounded-3xl bg-gray-800 after:pointer-events-none after:absolute after:inset-0 after:z-10 after:rounded-3xl after:outline after:outline-2 after:-outline-offset-2 after:outline-white/20 after:content-['']",
         className,
       )}
+      initial={{ opacity: 0, y: 60 }}
+      whileInView={{ opacity: 1, y: 0, transition: { duration: 0.6, delay } }}
+      viewport={{
+        once: true,
+      }}
       {...other}
     >
       <div
@@ -22,7 +31,7 @@ const Card = ({
         }}
       ></div>
       {children}
-    </div>
+    </motion.div>
   );
 };
 
