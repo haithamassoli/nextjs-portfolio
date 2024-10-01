7e61cc21627903e64e11d8f8e0d6dbdca8f534df
haithamassoli <haitham.b.assoli@gmail.com>
2024-10-01 11:23:39 +0300

    feat: enhance animation for TapeSvg in homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { FeaturedProject } from "@/sections/FeaturedProject";
@@ -29,7 +29,18 @@ export default function Home() {
           ))}
         </div>
       </div> */}
-      <TapeSvg className="w-full select-none bg-repeat-x grayscale transition-all duration-500 ease-in-out hover:filter-none" />
+      <div className="flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] md:mb-0">
+        <div className="group flex flex-none animate-move-left [animation-duration:30s]">
+          {[...new Array(2)].fill(0).map((_, idx) => (
+            <Fragment key={idx}>
+              <div className="transition duration-300">
+                <TapeSvg className="mt-6 select-none grayscale transition-all duration-500 ease-in-out group-hover:filter-none" />
+              </div>
+            </Fragment>
+          ))}
+        </div>
+      </div>
+      {/* <TapeSvg className="hidden w-full select-none bg-repeat-x grayscale transition-all duration-500 ease-in-out hover:filter-none md:block" /> */}
       <Jobs />
       <FeaturedProject />
       <Projects />
