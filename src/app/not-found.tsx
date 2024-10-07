86d875fd042891f368286f3df4fe1496d14ef374
haithamassoli <haitham.b.assoli@gmail.com>
2024-10-07 12:12:53 +0300

    improve not found page animation

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,3 @@
-"use client";
-
-import { motion } from "framer-motion";
 import Link from "next/link";
 
 const NotFoundPage = () => {
@@ -8,38 +5,18 @@ const NotFoundPage = () => {
     <div>
       <title>Page Not Found</title>
       <main className="flex min-h-screen flex-col items-center justify-center">
-        <motion.h1
-          className="text-[clamp(100px,25vw,200px)] leading-none text-emerald-500"
-          initial={{ opacity: 0, y: 120 }}
-          animate={{ opacity: 1, y: 0, transition: { duration: 0.6 } }}
-        >
+        <h1 className="letter-animation text-[clamp(100px,25vw,200px)] leading-none text-emerald-500">
           404
-        </motion.h1>
-        <motion.h2
-          className="text-[clamp(30px,5vw,50px)] font-normal"
-          initial={{ opacity: 0, y: 120 }}
-          animate={{
-            opacity: 1,
-            y: 0,
-            transition: { duration: 0.6, delay: 0.2 },
-          }}
-        >
+        </h1>
+        <h2 className="not-found-animation text-[clamp(30px,5vw,50px)] font-normal">
           Page Not Found
-        </motion.h2>
+        </h2>
         <Link href="/">
-          <motion.div
-            className="flex w-full justify-center"
-            initial={{ opacity: 0, y: 120 }}
-            animate={{
-              opacity: 1,
-              y: 0,
-              transition: { duration: 0.6, delay: 0.4 },
-            }}
-          >
+          <div className="not-found-button-animation flex w-full justify-center">
             <button className="btn mx-auto mt-12 rounded border border-green-400 bg-transparent px-4 py-2 text-green-400 transition-all duration-300 hover:bg-green-400 hover:bg-opacity-10">
               Go Home
             </button>
-          </motion.div>
+          </div>
         </Link>
       </main>
     </div>
