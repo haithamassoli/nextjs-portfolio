ed8100f4eed8794d19d53544bb03d4a455e2d3ce
haithamassoli <haitham.b.assoli@gmail.com>
2024-10-11 13:32:00 +0300

    text area component

---
+++ src/components/ui/textarea.tsx
@@ -0,0 +1,24 @@
+import { forwardRef, TextareaHTMLAttributes } from "react";
+
+import { cn } from "@/libs/utils";
+
+export interface TextareaProps
+  extends TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <textarea
+        className={cn(
+          "flex min-h-[80px] w-full rounded-md border border-slate-600 bg-slate-800 px-3 py-2 text-sm ring-offset-slate-950 placeholder:text-slate-400 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-emerald-300 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+Textarea.displayName = "Textarea";
+
+export { Textarea };
